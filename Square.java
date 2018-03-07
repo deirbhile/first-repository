7721cb5a3a1f903af54df535adca0148b9a51d46
DEIRBHILE TUOHY - STUDENT <G00359139@gmit.ie>
2018-03-07 13:54:14 +0000

    week 22 lab

---
+++ Square.java
@@ -0,0 +1,29 @@
+/**
+Name: Square
+Description: Draw a square
+Written on: 7/3/18
+Written by:
+
+*/
+import java.util.Scanner;
+ public class Square{
+ public static void main(String[] args){
+
+	 Scanner input = new Scanner(System.in);
+
+	 System.out.print("Enter square size: ");
+	 int size = input.nextInt();
+
+	 squareOfAsterisks(size);
+
+ }//end main
+
+  public static void squareOfAsterisks(int size){
+	  for(int row = 1; row <= size; row++){
+	  for(int col = 1; col <= size; col++)
+	   System.out.print("*");
+
+	   System.out.println();
+   }
+}
+}
