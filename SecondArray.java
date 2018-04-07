30119367c7ddf6e4c6506c49ff8f09caeb127952
deirbhile <36477530+deirbhile@users.noreply.github.com>
2018-04-07 12:54:30 +0100

    Project Work

--- SecondArray.java
+++ SecondArray.java
@@ -1,31 +1,8 @@
-/**
-Name: Second Array
-Description: take integer value and return it in reverse
-Created on: 14/03/18
-Created by: Deirbhile Tuohy
-**/
-public class SecondArray{
-public static void main(String[] args){
+﻿C:\Users\Deirbhile\Downloads\SecondArray.java:19: error: cannot find symbol
+		Index.main(null);
+		^
+  symbol:   variable Index
+  location: class SecondArray
+1 error
 
-int[] x = int{128, 132, 8, 156, 18};
-
-
-System.out.println("Index\tValue");
-}
-for(int i = 0; i < x.length; i++){;
-System.out.println(i+"\t"+x[i]);
-
-
-Index.main(null);
-
-
-}
-System.out.println("");
-}
-
-
-
-
-
-
-
+Tool completed with exit code 1
---
+++ TryCatch.java
@@ -0,0 +1,19 @@
+public class TryCatch{
+public static void main(String[] args){
+
+int[] x = {7, 6, 11, 15, 19};
+
+try{
+System.out.println("The value stored in index 5 is: "+x[5]);
+}catch(ArrayIndexOutOfBoundsException e){
+System.out.println("There was an ArrayOutOfBoundsException");
+}catch(Exception e){
+System.out.println("Warning: Some other exception");
+}
+
+System.out.println("End of Program");
+Index.main(null);
+
+}
+
+}
